d3499cae765664c9190af5f517739011d6125b7d
TotevM <mtotev53@gmail.com>
2025-05-11 20:26:38 +0300

    ex25

---
+++ 2022/May/May_2022_Solutions/zad25/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace zad25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Dictionary<int, int> dictionary = new Dictionary<int, int>();
+                string nInput = Console.ReadLine()!;
+                if (string.IsNullOrWhiteSpace(nInput))
+                {
+                    throw new ArgumentNullException();
+                }
+                int n = int.Parse(nInput);
+
+                if (n <= 0)
+                {
+                    throw new ArgumentException("Въведи положително цяло число");
+                }
+
+                for (int i = 0; i < n; i++)
+                {
+                    string input = Console.ReadLine()!;
+
+                    if (string.IsNullOrWhiteSpace(input))
+                    {
+                        throw new ArgumentNullException();
+                    }
+                    int num = int.Parse(input);
+                    if (num <= 0)
+                    {
+                        throw new ArgumentException("Въведи положително цяло число");
+                    }
+
+                    if (dictionary.ContainsKey(num))
+                    {
+                        dictionary[num]++;
+                    }
+                    else
+                    {
+                        dictionary[num] = 1;
+                    }
+                }
+
+                foreach (var pair in dictionary)
+                {
+                    Console.WriteLine($"Число: {pair.Key}, брой: {pair.Value}");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Моля, въведете валидно цяло число.");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Моля, въведете валидно цяло число.");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Грешка: " + ex.Message);
+            }
+        }
+    }
+}
