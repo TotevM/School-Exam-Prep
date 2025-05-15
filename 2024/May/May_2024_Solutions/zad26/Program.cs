20d29fd9f549b6dc52e43e1d131986bc05fdf8c4
TotevM <mtotev53@gmail.com>
2025-05-15 20:17:15 +0300

    ex26

---
+++ 2024/May/May_2024_Solutions/zad26/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace zad26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = null;
+            int index = input.IndexOf("z");
+            Console.WriteLine(index);
+        }
+
+        public static void zad26(int i)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
